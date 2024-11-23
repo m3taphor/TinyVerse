e6c0b374261ce4b1bee8effe03cc98322ac9308e
m3taphor <im3taphor@proton.me>
2024-11-23 11:15:28 +0530

    [update] encode Unix (LF)

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ source venv/bin/activate
 
 if [ ! -f "venv/installed" ]; then
     if [ -f "requirements.txt" ]; then
-		echo "Installing wheel for faster installing"
-		pip3 install wheel
+        echo "Installing wheel for faster installing"
+        pip3 install wheel
         echo "Installing dependencies..."
         pip3 install -r requirements.txt
         touch venv/installed
@@ -23,14 +23,13 @@ else
 fi
 
 if [ ! -f ".env" ]; then
-	echo "Copying configuration file"
-	cp .env-example .env
+    echo "Copying configuration file"
+    cp .env-example .env
 else
-	echo "Skipping .env copying"
+    echo "Skipping .env copying"
 fi
 
 echo "Starting the bot..."
 python3 main.py
 
-echo "done"
-echo "PLEASE EDIT .ENV FILE"
+echo "Script Stopped"
