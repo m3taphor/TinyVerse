74a1ebfe703f1dd42848eca893776fc0bb8736f5
m3taphor <im3taphor@proton.me>
2024-12-05 11:14:14 +0530

    [update] boost fix, new headers
    Added New Headers

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -1,6 +1,7 @@
 headers = {
     "Host": "api.tonverse.app",
     "Connection": "keep-alive",
+    "X-Application-Version": "0.6.30",
     "sec-ch-ua-platform": "\"Windows\"",
     "X-Requested-With": "XMLHttpRequest",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
