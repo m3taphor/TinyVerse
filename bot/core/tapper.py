a1deb25cecd3df6eb285142ea7a7eb2dec8f0e46
m3taphor <im3taphor@proton.me>
2024-12-19 16:49:15 +0530

    [update] 'lower' error fix

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -113,7 +113,8 @@ class Tapper:
             tg_web_data = unquote(string=auth_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0])
             
             user_data = unquote(tg_web_data.split('&user=')[1])
-            username = user_data.split('"username":"')[1].split('"')[0].lower() if '"username":"' in user_data else None
+            print(user_data)
+            username = user_data.split('"username":"')[1].split('"')[0].lower() if '"username":"' in user_data else 'null'
             
             me = await self.tg_client.get_me()
             self.tg_client_id = me.id
